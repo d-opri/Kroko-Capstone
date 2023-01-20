4be41b988edfa37a2617ab301b62bffb77a1f5f4
Daniela Opri <danieo.ux@gmail.com>
2023-01-20 09:40:54 +0100

    add activity.js, update index.js

--- pages/activity.js
+++ pages/activity.js
@@ -1,28 +1,64 @@
-import styled from "styled-components";
 import React from "react";
 import listItems from "../db";
+import styled from "styled-components";
+import List from "../components/List";
 
-export default function List({ children }) {
+export default function Activity() {
   return (
     <>
-      <ListLayout>
-        {listItems.map((item, { id }) => (
-          <li key={id}>
-            <ListItemLayout>
-              <ListTitleText>{item.title}</ListTitleText>
-
-              <AmountText
-                style={{ color: item.amount.includes("+") ? "green" : "red" }}
-              >
-                {item.amount}
-              </AmountText>
-            </ListItemLayout>
-          </li>
-        ))}
-      </ListLayout>
+      <Heading>
+        <Title>Activity</Title>
+      </Heading>
+      <List />
     </>
   );
 }
+
+const Heading = styled.div`
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 13px;
+
+  position: absolute;
+  width: 92px;
+  height: 28px;
+  left: calc(50% - 92px / 2 + 0.5px);
+  top: 107px;
+`;
+
+const Title = styled.h1`
+  /* Activity */
+
+  width: 92px;
+  height: 28px;
+
+  /* Heading/6/Bold */
+
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 28px;
+  /* identical to box height, or 117% */
+
+  display: flex;
+  align-items: center;
+
+  /* Black */
+
+  color: #000000;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
 
 const ListLayout = styled.div`
   /* Auto layout */
