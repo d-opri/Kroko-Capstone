63136554f4f8cbe09a8126ec72658dc0e0ee11b3
Daniela Opri <danieo.ux@gmail.com>
2023-01-20 16:35:07 +0100

    correct styling

--- components/List.js
+++ components/List.js
@@ -23,89 +23,36 @@ export default function List() {
 }
 
 const ListLayout = styled.div`
-  /* Auto layout */
   list-style: none;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 0px;
   gap: 16px;
-
-  position: absolute;
-  width: 268px;
-  height: 464px;
-  left: calc(50% - 268px / 2);
-  bottom: -52px;
+  margin: auto;
+  padding-top: 80px;
 `;
 
 const ListItemLayout = styled.div`
-  /* Auto layout */
-
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
   align-items: center;
-  padding: 12px 8px;
-  gap: 134px;
-
-  width: 288px;
+  width: 300px;
   height: 44px;
-
-  /* Gray */
-
   background: #e1e1e1;
   border-radius: 10px;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  align-self: stretch;
-  flex-grow: 0;
 `;
 
 const ListTitleText = styled.h3`
-  width: 84px;
+  width: 100%;
   height: 20px;
-
-  font-family: "Inter";
-  font-style: normal;
-  font-weight: 600;
   font-size: 15px;
-  line-height: 20px;
-  /* identical to box height, or 133% */
-
   text-align: center;
-  letter-spacing: 0.005em;
-
   color: #000000;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
 `;
 
 const AmountText = styled.p`
-  width: 64px;
+  width: 100%;
   height: 20px;
-
-  font-family: "Inter";
-  font-style: normal;
-  font-weight: 500;
   font-size: 13px;
-  line-height: 20px;
-  /* identical to box height, or 154% */
-
   text-align: center;
-  letter-spacing: 0.005em;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 1;
-  flex-grow: 0;
 `;
-
-console.log(listItems);
--- pages/activity.js
+++ pages/activity.js
@@ -5,56 +5,16 @@ import List from "../components/List";
 export default function Activity() {
   return (
     <>
-      <Heading>
-        <Title>Activity</Title>
-      </Heading>
+      <Title>Activity</Title>
+
       <List />
     </>
   );
 }
 
-const Heading = styled.div`
-  /* Auto layout */
-
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
-  gap: 13px;
-
-  position: absolute;
-  width: 92px;
-  height: 28px;
-  left: calc(50% - 92px / 2 + 0.5px);
-  top: 107px;
-`;
-
 const Title = styled.h1`
-  /* Activity */
-
-  width: 92px;
-  height: 28px;
-
-  /* Heading/6/Bold */
-
-  font-family: "DM Sans";
-  font-style: normal;
   font-weight: 700;
   font-size: 24px;
-  line-height: 28px;
-  /* identical to box height, or 117% */
-
-  display: flex;
-  align-items: center;
-
-  /* Black */
-
-  color: #000000;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
+  text-align: center;
+  padding-top: 50px;
 `;
