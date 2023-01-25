88f7fa7b400f805c69b922f8b41e3ee5a5e948e7
Daniela Opri <danieo.ux@gmail.com>
2023-01-25 13:13:19 +0100

    map over list items to add radio input

--- components/AddParticipants.js
+++ components/AddParticipants.js
@@ -8,12 +8,40 @@ function AddParticipants() {
 
   const [isPaid, setIsPaid] = useState(false);
 
+  function handleAddList() {
+    setAddName("Add Name");
+    setList([
+      ...list,
+      {
+        label: addName,
+        value: addName,
+      },
+    ]);
+  }
+
+  function handleChange() {
+    setIsPaid(!isPaid);
+  }
+
   return (
     <>
-      <input type='text' />
-      <button>Add Participant</button>
-      <input type='radio' name='participants' />
-      <label htmlFor='participants'></label>
+      <input
+        value={addName}
+        onChange={(e) => setAddName(e.target.value)}
+        type='text'
+      />
+      <button onClick={handleAddList}>Add Participant</button>
+      {list.map((item) => (
+        <>
+          <input
+            type='radio'
+            value={item.value}
+            onChange={handleChange}
+            name='participants'
+          />
+          <label htmlFor='participants'>{item.label}</label>
+        </>
+      ))}
     </>
   );
 }
